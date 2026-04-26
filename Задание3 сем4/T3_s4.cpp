3b7593f5adfc519d6f135576b0ca453a22f2c3e1
KirillMurtazin <kirillmurtazin24@gmail.com>
2026-04-26 23:13:57 +0300

    Доработанное задание 3

--- Задание3 сем4/T3_s4.cpp
+++ Задание3 сем4/T3_s4.cpp
@@ -3,11 +3,36 @@
 #include <map>
 #include <algorithm>
 #include <sstream>
+#include <iterator>
 #include <locale.h>
 
 using namespace std;
 
-int main() 
+void printVector(const vector<int>& v, const string& message)
+{
+    cout << message;
+    for (vector<int>::const_iterator it = v.begin(); it != v.end(); ++it)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+// Функция для вывода мультиотображения
+void printMultimap(const multimap<int, int>& m, const string& message)
+{
+    cout << message << endl;
+    cout << "Ключ (последняя цифра) -> Значение (элемент)" << endl;
+    cout << "\n" << "********************************************" << endl;
+
+    for (multimap<int, int>::const_iterator it = m.begin(); it != m.end(); ++it)
+    {
+        cout << it->first << " -> " << it->second << endl;
+    }
+    cout << "********************************************" << endl;
+}
+
+int main()
 {
     setlocale(LC_ALL, "RU");
     vector<int> V;
@@ -16,42 +41,30 @@ int main()
     cout << "Введите целые числа через пробел: ";
     getline(cin, input);
 
-    // Разбираем введенную строку
     istringstream iss(input);
-    int num;
-    while (iss >> num) {
-        V.push_back(num);
-    }
+    istream_iterator<int> start(iss), end;
+    V.assign(start, end);
 
-    if (V.empty()) {
+    if (V.empty())
+    {
         cerr << "Ошибка: не введено ни одного числа" << endl;
         return 1;
     }
 
-    cout << "Исходный вектор: ";
-    for (vector<int>::iterator it = V.begin(); it != V.end(); ++it) {
-        cout << *it << " ";
-    }
-    cout << endl << endl;
+    printVector(V, "Исходный вектор: ");
+    cout << endl;
 
     // Создаем мультиотображение для группировки по последней цифре
     multimap<int, int> M;
 
     // Заполняем мультиотображение
-    for (vector<int>::iterator it = V.begin(); it != V.end(); ++it) {
+    for (vector<int>::const_iterator it = V.begin(); it != V.end(); ++it)
+    {
         int lastDigit = abs(*it) % 10;  // Последняя цифра (берем абсолютное значение для отрицательных чисел)
         M.insert(make_pair(lastDigit, *it));
     }
 
-    cout << "Результат группировки (multimap):" << endl;
-    cout << "Ключ (последняя цифра) -> Значение (элемент)" << endl;
-    cout << "\n" << "********************************************" << endl;
-
-    // Выводим содержимое мультиотображения с помощью итераторов
-    for (multimap<int, int>::iterator it = M.begin(); it != M.end(); ++it) {
-        cout << it->first << " -> " << it->second << endl;
-    }
-    cout << "********************************************" << endl;
+    printMultimap(M, "Результат группировки (multimap):");
 
     return 0;
 }
